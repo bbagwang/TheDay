d9834d6b545606d6919789060faa45f6896db46d
BBAGWANG <bbagwang@gmail.com>
2020-08-09 17:22:47 +0900

    TDPlayerController 추가

---
+++ Source/TheDay/System/TDPlayerController.cpp
@@ -0,0 +1,8 @@
+// Copyright BBAGWANG SOFT, Inc. All Rights Reserved.
+
+#include "TDPlayerController.h"
+
+ATDPlayerController::ATDPlayerController()
+{
+
+}
---
+++ Source/TheDay/System/TDPlayerController.h
@@ -0,0 +1,16 @@
+// Copyright BBAGWANG SOFT, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "TDPlayerController.generated.h"
+
+UCLASS()
+class THEDAY_API ATDPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+public:
+	ATDPlayerController();
+};
