55955835619a6769c7c8f3d9119401ab9684e5ad
BBAGWANG <bbagwang@gmail.com>
2020-08-10 01:40:29 +0900

    TD HUD 추가

---
+++ Source/TheDay/System/TDHUD.h
@@ -0,0 +1,16 @@
+// Copyright BBAGWANG SOFT, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "TDHUD.generated.h"
+
+UCLASS()
+class THEDAY_API ATDHUD : public AHUD
+{
+	GENERATED_BODY()
+
+public:
+	ATDHUD();
+};
