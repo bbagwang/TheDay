20479f30ad8cfb8a9d9ca05e3f7af55834eddbf2
BBAGWANG <bbagwang@gmail.com>
2020-09-07 23:04:46 +0900

    TD Character Movement Component 기본적인 움직임 기능 모두 사용하도록 설정

--- Source/TheDay/Character/Component/TDCharacterMovementComponent.cpp
+++ Source/TheDay/Character/Component/TDCharacterMovementComponent.cpp
@@ -2,3 +2,30 @@
 
 #include "TDCharacterMovementComponent.h"
 
+UTDCharacterMovementComponent::UTDCharacterMovementComponent()
+{
+	NavAgentProps.bCanCrouch = true;
+	NavAgentProps.bCanFly = true;
+	NavAgentProps.bCanJump = true;
+	NavAgentProps.bCanSwim = true;
+	NavAgentProps.bCanWalk = true;
+	
+	GetNavAgentPropertiesRef().bCanCrouch = true;
+	GetNavAgentPropertiesRef().bCanFly = true;
+	GetNavAgentPropertiesRef().bCanJump = true;
+	GetNavAgentPropertiesRef().bCanSwim = true;
+	GetNavAgentPropertiesRef().bCanWalk = true;
+
+}
+
+void UTDCharacterMovementComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+void UTDCharacterMovementComponent::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+}
--- Source/TheDay/Character/Component/TDCharacterMovementComponent.h
+++ Source/TheDay/Character/Component/TDCharacterMovementComponent.h
@@ -6,12 +6,15 @@
 #include "GameFramework/CharacterMovementComponent.h"
 #include "TDCharacterMovementComponent.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class THEDAY_API UTDCharacterMovementComponent : public UCharacterMovementComponent
 {
 	GENERATED_BODY()
 	
+public:
+	UTDCharacterMovementComponent();
+
+	virtual void BeginPlay() override;
+	virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) override;
+
 };
