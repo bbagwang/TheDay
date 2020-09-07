4353cadedf8f46e8e9e753a660ae66483f81bb05
BBAGWANG <bbagwang@gmail.com>
2020-09-07 23:11:06 +0900

    Melee Weapon 추가

---
+++ Source/TheDay/Weapon/MeleeWeapon.h
@@ -0,0 +1,17 @@
+// Copyright BBAGWANG SOFT, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Weapon/Weapon.h"
+#include "MeleeWeapon.generated.h"
+
+UCLASS()
+class THEDAY_API AMeleeWeapon : public AWeapon
+{
+	GENERATED_BODY()
+
+public:
+	AMeleeWeapon();
+
+};
