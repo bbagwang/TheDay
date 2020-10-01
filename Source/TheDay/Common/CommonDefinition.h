1775645fe93c08ecca08a75d3305e2de9e75fcac
BBAGWANG <bbagwang@gmail.com>
2020-10-01 13:06:22 +0900

    TD GameInstance와 EventManager 관련 Wrapper 메크로 추가

--- Source/TheDay/Common/CommonDefinition.h
+++ Source/TheDay/Common/CommonDefinition.h
@@ -2,4 +2,13 @@
 
 #pragma once
 
+#include <System/TDGameInstance.h>
+
+#pragma region TraceChannel
 #define WeaponTraceChannel ECC_GameTraceChannel1
+#pragma endregion
+
+#pragma region Globals
+#define TDGAME_INSTANCE(object) (Cast<UTDGameInstance>(object->GetWorld()->GetGameInstance()))
+#define EVENT_MANAGER(object) (TDGAME_INSTANCE(object)->EventManager)
+#pragma endregion
