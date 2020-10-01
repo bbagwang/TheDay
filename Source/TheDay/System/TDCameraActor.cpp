581a1075131261c8f9b15e3d11e63ba3db3055a3
BBAGWANG <bbagwang@gmail.com>
2020-10-01 13:00:14 +0900

    CineCameraActor 기반 TDCameraActor 추가

---
+++ Source/TheDay/System/TDCameraActor.cpp
@@ -0,0 +1,8 @@
+// Copyright BBAGWANG SOFT, Inc. All Rights Reserved.
+
+#include "TDCameraActor.h"
+
+ATDCameraActor::ATDCameraActor(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+
+}
---
+++ Source/TheDay/System/TDCameraActor.h
@@ -0,0 +1,17 @@
+// Copyright BBAGWANG SOFT, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CineCameraActor.h"
+#include "TDCameraActor.generated.h"
+
+UCLASS()
+class THEDAY_API ATDCameraActor : public ACineCameraActor
+{
+	GENERATED_BODY()
+	
+public:
+	ATDCameraActor(const FObjectInitializer& ObjectInitializer);
+
+};
