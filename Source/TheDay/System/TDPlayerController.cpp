fed1fde768d1b548764cb08a742451a333aecd8c
BBAGWANG <bbagwang@gmail.com>
2020-10-01 13:24:45 +0900

    TD Player Controller에 ESC 키 관련 바인딩 추가

--- Source/TheDay/System/TDPlayerController.cpp
+++ Source/TheDay/System/TDPlayerController.cpp
@@ -1,8 +1,48 @@
 // Copyright BBAGWANG SOFT, Inc. All Rights Reserved.
 
 #include "TDPlayerController.h"
+#include "Common/CommonNameSpace.h"
 
 ATDPlayerController::ATDPlayerController()
 {
-
+	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bStartWithTickEnabled = true;
 }
+
+void ATDPlayerController::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+}
+
+void ATDPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ATDPlayerController::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+}
+
+void ATDPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+	check(InputComponent);
+
+	InputComponent->BindAction(InputKeyName::ESC, IE_Pressed, this, &ATDPlayerController::Input_StartEscape);
+	InputComponent->BindAction(InputKeyName::ESC, IE_Released, this, &ATDPlayerController::Input_EndEscape);
+}
+
+#pragma region Input
+void ATDPlayerController::Input_StartEscape()
+{
+
+	Receive_Input_StartEscape();
+}
+
+void ATDPlayerController::Input_EndEscape()
+{
+
+	Receive_Input_EndEscape();
+}
+#pragma endregion
--- Source/TheDay/System/TDPlayerController.h
+++ Source/TheDay/System/TDPlayerController.h
@@ -13,4 +13,19 @@ class THEDAY_API ATDPlayerController : public APlayerController
 	
 public:
 	ATDPlayerController();
+	
+	virtual void PostInitializeComponents() override;
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaSeconds) override;
+	virtual void SetupInputComponent() override;
+
+#pragma region Input
+	void Input_StartEscape();
+	void Input_EndEscape();
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void Receive_Input_StartEscape();
+	UFUNCTION(BlueprintImplementableEvent)
+	void Receive_Input_EndEscape();
+#pragma endregion
 };
