938d6e75f3fa470ec8978bd4f658757118499cce
BBAGWANG <bbagwang@gmail.com>
2020-10-01 13:02:13 +0900

    CharacterBoneName, WeaponSOcketName, InputKeyName 관련 상수 추가

--- Source/TheDay/Common/CommonNameSpace.h
+++ Source/TheDay/Common/CommonNameSpace.h
@@ -2,8 +2,42 @@
 
 #pragma once
 
+namespace CharacterBoneName
+{
+	static const FName HAND_L = TEXT("hand_l");
+	static const FName HAND_R = TEXT("hand_r");
+}
+
 namespace CharacterSocketName
 {
-	static const FName HAND_L_SOCKET = "hand_l_Socket";
-	static const FName HAND_R_SOCKET = "hand_r_Socket";
+	static const FName HAND_L_SOCKET = TEXT("hand_l_Socket");
+	static const FName HAND_R_SOCKET = TEXT("hand_r_Socket");
+}
+
+namespace WeaponSocketName
+{
+	static const FName LEFT_HAND_IK_SOCKET = TEXT("LeftHandIKSocket");
+}
+
+namespace InputKeyName
+{
+	//ACTION
+	static const FName JUMP = TEXT("Jump");
+	static const FName CROUCH = TEXT("Crouch");
+	static const FName SPRINT = TEXT("Sprint");
+	static const FName AIM = TEXT("Aim");
+	static const FName ATTACK = TEXT("Attack");
+	static const FName INTERACTION = TEXT("Interaction");
+
+	//SPECIAL ACTION
+	static const FName ESC = TEXT("Escape");
+
+	//AXIS
+	static const FName MOVE_FORWARD = TEXT("MoveForward");
+	static const FName MOVE_RIGHT = TEXT("MoveRight");
+	static const FName TURN = TEXT("Turn");
+	static const FName TURN_RATE = TEXT("TurnRate");
+	static const FName LOOKUP = TEXT("LookUp");
+	static const FName LOOKUP_RATE = TEXT("LookUpRate");
+	
 }
